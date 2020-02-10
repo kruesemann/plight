3fe588cf00bbdac1fd408a34cb9120dac4a50701
Jonas Krüsemann <jonas.kruesemann@t-online.de>
2020-02-10 21:19:00 +0100

    add: uniform for testing

--- plight/source/main/plight.cpp
+++ plight/source/main/plight.cpp
@@ -7,6 +7,9 @@
 #include "plight/include/graphics/renderer.h"
 #include "plight/include/graphics/shader_manager.h"
 
+#include "glew/include/glew.h"
+#include "glfw/include/glfw3.h"
+
 
 void test()
 {
@@ -16,20 +19,31 @@ void test()
 
     renderer.setBlendFunction(Plight::Graphics::EBlendFactor::CurrentAlpha,
                               Plight::Graphics::EBlendFactor::OneMinusCurrentAlpha);
+    renderer.setEnableAlphaBlending(true);
 
-    Plight::String positionAttribute("a_position");
     std::vector<Plight::Graphics::Attribute> attributes = {
-        Plight::Graphics::Attribute{positionAttribute, 2, std::vector<float>{1.0f, 1.0f,    // top right
-                                                                             1.0f, 0.0f,    // bottom right
-                                                                             0.0f, 0.0f,    // bottom left
-                                                                             0.0f, 1.0f}}   // top left
+        Plight::Graphics::Attribute{Plight::String("a_position"), 2, std::vector<float>{1.0f, 1.0f,     // top right
+                                                                                        1.0f, 0.0f,     // bottom right
+                                                                                        0.0f, 0.0f,     // bottom left
+                                                                                        0.0f, 1.0f}},   // top left
+        Plight::Graphics::Attribute{Plight::String("a_color"), 3, std::vector<float>{1.0f, 0.0f, 0.0f,
+                                                                                     1.0f, 1.0f, 0.0f,
+                                                                                     0.0f, 1.0f, 0.0f,
+                                                                                     0.0f, 0.0f, 1.0f}}
     };
     std::vector<int> indices = {0, 1, 3, 1, 2, 3};
 
     auto const renderData = Plight::Graphics::RenderDataFactory::create(shaderManager, Plight::String("test_shader"), attributes, indices);
 
+    auto const location = glGetUniformLocation(renderData.m_shaderProgramId, "u_opacity");
+    if (location < 0)
+        return;
+
     while (window.pollEvents())
     {
+        glUseProgram(renderData.m_shaderProgramId);
+        glUniform1f(location, sin(glfwGetTime()) / 2.0f + 0.5f);
+        
         renderer.render(renderData);
         window.update();
     }
